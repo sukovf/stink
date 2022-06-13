e60041849ab208ff353e7afd5656c8bdbe3a1911
sukovf <msoucek@seznam.cz>
2022-06-13 19:28:26 +0200

    Initial commit

---
+++ src/Controller/MainController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ *
+ */
+class MainController extends AbstractController
+{
+	/**
+	 * @Route("/", name="main")
+	 */
+	public function index(): Response
+	{
+		return new Response('<html><body>Hokus...</body></html>');
+	}
+}
